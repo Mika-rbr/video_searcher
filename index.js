67c851cdfb031b85271b1544322fec41594ada51
Mika-rbr <86625074+Mika-rbr@users.noreply.github.com>
2022-06-08 13:07:34 -0400

    final commit

--- index.js
+++ index.js
@@ -73,35 +73,6 @@ const addNewKeyword = (label, keyword) => {
     resetKeywordsUl();
 };
 
-// We reload the articles depends of the currentKeywords
-// TODO: Modify this function to display only articles that contain at least one of the selected keywords.
-const reloadArticles = () => {
-    document.querySelector('.articlesList').innerHTML = '';
-    
-    const articlesToShow = data.articles.filter((article) => 
-        article.tags.some((tag) => currentKeywords.includes(tag))
-    );
-
-    if (currentKeywords.length === 0) {
-        articlesToShow = data.articles;
-    }
-
-
-
-    articlesToShow.forEach((article) => {
-        document.querySelector('.articlesList').innerHTML += `
-            <article>
-                <h2>${article.titre}</h2>
-            </article>
-        `;
-    });
-};
-
-// We empty the content from the <ul> under the text input
-const resetKeywordsUl = () => {
-    document.querySelector('.inputKeywordsHandle ul').innerHTML = '';
-};
-
 // We add a new article. The argument is an object with a title
 const addNewArticle = (article) => {
     document.querySelector('.articlesList').innerHTML += `
@@ -116,40 +87,77 @@ const resetInput = () => {
     document.querySelector("input[type='text']").value = "";
 };
 
+// We empty the content from the <ul> under the text input
+const resetKeywordsUl = () => {
+    document.querySelector('.inputKeywordsHandle ul').innerHTML = '';
+};
+
 // Clean a keyword to lowercase and without special characters
 // TODO: Make the cleaning
-const cleanedKeyword = (keyword) => {
-    const cleanedKeyword = keyword.replace(/[^\w\s]/gi, '').toLowerCase();
+const cleanedKeyword = (keyword) => keyword.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
 
-    return cleanedKeyword;
+// We reload the articles depends of the currentKeywords
+// TODO: Modify this function to display only articles that contain at least one of the selected keywords.
+const reloadArticles = () => {
+    document.querySelector('.articlesList').innerHTML = ''
+    
+    let articlesToShow;
+
+    if (currentKeywords.length === 0) {
+        articlesToShow = data.articles;
+    } else {
+        articlesToShow = data.articles.filter((article) => article.tags.some((tag) => currentKeywords.includes(tag)));
+    }
+
+    articlesToShow.forEach((article) => {
+        document.querySelector('.articlesList').innerHTML += `
+            <article>
+                <h2>${article.titre}</h2>
+            </article>
+        `;
+    });
 };
+
 
 // TODO: Modify this function to show the keyword containing a part of the word inserted into the form (starting autocompletion at 3 letters).
 // TODO: We also show all the words from the same category than this word.
 // TODO: We show in first the keyword containing a part of the word inserted.
 // TODO: If a keyword is already in the list of presents hashtags (checkbox list), we don't show it.
 const showKeywordsList = (value) => {
-    // Starting at 3 letters inserted in the form, we do something
+    // Starting at 3 letters inserted in the forme, we start to do something
     if (value.length >= 3) {
-        const keyWordUl = document.querySelector(".inputKeywordsHandle ul");
-        allKeywords.forEach((keyword) => {
-            if (keyword.toLowerCase().includes(value)) {
-                document.querySelector("input[type='text']").value = keyword;
-                keyWordUl.innerHTML += `
-                    <li onclick="addNewKeyword('${keyword}', '${cleanedKeyword(keyword)}')">${keyword}</li>
-                    `;
-                console.log(keyWordUl.innerHTML);
-            }
-        });
+      const keyWordUl = document.querySelector(".inputKeywordsHandle ul");
+      resetKeywordsUl();
+  
+      let addNewKeywordOnUl = (keyword) => keyWordUl.innerHTML += `<li onclick="addNewKeyword('${keyword}', '${cleanedKeyword(keyword)}')">${keyword}</li>`;
+  
+      // To return a word in PascalCase
+      let PascalCaseValue = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+      // We prepare the word the user want to ""
+      let wantedKeyword = ""
+  
+      // For each of all keywords, if the keyword contains what the user type, this is the word the user want. So we put this keyword to wantedKeyword and we add it in first under the list (if he is not present in the keywords already added)
+      allKeywords.forEach(keyword => {
+        if (keyword.includes(PascalCaseValue(value)) && !keywords.some((alreadyAddKeyword) => alreadyAddKeyword.includes(value.toLowerCase()))) {
+          wantedKeyword = keyword
+          addNewKeywordOnUl(wantedKeyword)
+        }
+      })
+  
+      // We search the categorie of the keyword typed by the user
+      let categorieSearched = keywordsCategories.filter(categorie => {
+        return categorie.keywords.some((keyword) => keyword.includes(wantedKeyword))
+      })
+  
+      // For each keyword of the categorie of the word typped by the user, if the world isn't already in the keyword list and the word isn't the word typed by the user, we add it under the input
+      categorieSearched[0].keywords.forEach(keywordFromTheCategory => {
+        if (!keywords.some((alreadyAddKeyword) => alreadyAddKeyword.includes(keywordFromTheCategory.toLowerCase())) && keywordFromTheCategory != wantedKeyword) {
+          addNewKeywordOnUl(keywordFromTheCategory)
+        }
+      })
+  
     }
-        resetKeywordsUl();
-        
-        // This will allow you to add a new element in the list under the text input
-        // On click, we add the keyword, like so:
-        // keyWordUl.innerHTML += `
-        //    <li onclick="addNewKeyword(`${keyword}`, `${cleanedKeyword(keyword)}`)">${keyword}</li>
-        // `;
-};
+  }
 
 // Once the DOM (you will see what is it next week) is loaded, we get back our form and we prevent the initial behavior of the navigator: reload the page when it's submitted. Then we call the function addNewKeyword() with 2 arguments: the label value to show and the checkbox value.
 window.addEventListener('DOMContentLoaded', () => {
